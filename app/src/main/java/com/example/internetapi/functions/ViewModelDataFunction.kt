4494b00290c538fddcef60f4bb8ae31502fdaa8f
Mariusz Staniak <mariusz.staniak@gmail.com>
2022-01-01 16:18:20 +0100

    extract view model function to abstract class

---
+++ app/src/main/java/com/example/internetapi/functions/ViewModelDataFunction.kt
@@ -0,0 +1,46 @@
+package com.example.internetapi.functions
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.internetapi.api.Resource
+import com.example.internetapi.models.UpdateAccountResponse
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+abstract class ViewModelDataFunction : ViewModel() {
+    fun <T> executeLiveDataList(repoFun: suspend () -> Response<List<T>>): MutableLiveData<Resource<List<T>>> {
+        val data = MutableLiveData<Resource<List<T>>>()
+        viewModelScope.launch {
+            data.postValue(Resource.loading(null))
+            repoFun.invoke()
+                .let {
+                    if (it.isSuccessful) {
+                        data.postValue(Resource.success(it.body()))
+                    } else {
+                        data.postValue(Resource.error(it.errorBody().toString(), null))
+                    }
+                }
+        }
+        return data
+    }
+
+    fun <T> executeLiveDataSingle(function: suspend () -> Response<T>): LiveData<Resource<T>> {
+        val data = MutableLiveData<Resource<T>>()
+        viewModelScope.launch {
+            function.invoke()
+                .let {
+                    if (it.isSuccessful) {
+                        Log.i("TAG", "updateAccount: SUCCESS")
+                        data.postValue(Resource.success(it.body()))
+                    } else {
+                        Log.e("TAG", "updateAccount: FAILURE")
+                        data.postValue(Resource.error(it.errorBody().toString(), null))
+                    }
+                }
+        }
+        return data
+    }
+}
--- app/src/main/java/com/example/internetapi/ui/viewModel/AccountViewModel.kt
+++ app/src/main/java/com/example/internetapi/ui/viewModel/AccountViewModel.kt
@@ -1,128 +1,53 @@
 package com.example.internetapi.ui.viewModel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.internetapi.api.Resource
+import com.example.internetapi.functions.ViewModelDataFunction
 import com.example.internetapi.models.*
 import com.example.internetapi.repository.AccountRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AccountViewModel @Inject constructor(private val accountRepository: AccountRepository) :
-    ViewModel() {
+    ViewModelDataFunction() {
 
     fun accountInvoices(accountId: Int): LiveData<Resource<List<AccountInvoice>>> {
-        val data = MutableLiveData<Resource<List<AccountInvoice>>>()
-        viewModelScope.launch {
-            data.postValue(Resource.loading(null))
-            accountRepository.getAccountInvoices(accountId)
-                .let {
-                    if (it.isSuccessful) {
-                        data.postValue(Resource.success(it.body()))
-                    } else {
-                        data.postValue(Resource.error(it.errorBody().toString(), null))
-                    }
-                }
-        }
-        return data
+        return executeLiveDataList { accountRepository.getAccountInvoices(accountId) }
     }
 
     fun getAccounts(): MutableLiveData<Resource<List<Account>>> {
-        val data = MutableLiveData<Resource<List<Account>>>()
-        viewModelScope.launch {
-            data.postValue(Resource.loading(null))
-            accountRepository.getAccounts()
-                .let {
-                    if (it.isSuccessful) {
-                        data.postValue(Resource.success(it.body()))
-                    } else {
-                        data.postValue(Resource.error(it.errorBody().toString(), null))
-                    }
-                }
-        }
-        return data
+        return executeLiveDataList { accountRepository.getAccounts() }
     }
 
+
     fun getAccountIncome(accountId: Int): LiveData<Resource<List<AccountIncome>>> {
-        val data = MutableLiveData<Resource<List<AccountIncome>>>()
-        viewModelScope.launch {
-            data.postValue(Resource.loading(null))
-            accountRepository.getAccountIncome(accountId)
-                .let {
-                    if (it.isSuccessful) {
-                        data.postValue(Resource.success(it.body()))
-                    } else {
-                        data.postValue(Resource.error(it.errorBody().toString(), null))
-                    }
-                }
-        }
-        return data
+        return executeLiveDataList { accountRepository.getAccountIncome(accountId) }
     }
 
     fun updateAccount(
         accountId: Int,
         updateAccountRequest: UpdateAccountRequest
-    ): MutableLiveData<Resource<UpdateAccountResponse>> {
-        val data = MutableLiveData<Resource<UpdateAccountResponse>>()
-        viewModelScope.launch {
-            accountRepository.updateAccount(accountId, updateAccountRequest)
-                .let {
-                    if (it.isSuccessful) {
-                        Log.i("TAG", "updateAccount: SUCCESS")
-                        data.postValue(Resource.success(it.body()))
-                    } else {
-                        Log.e("TAG", "updateAccount: FAILURE")
-                        data.postValue(Resource.error(it.errorBody().toString(), null))
-                    }
-                }
+    ): LiveData<Resource<UpdateAccountResponse>> {
+        return executeLiveDataSingle {
+            accountRepository.updateAccount(
+                accountId,
+                updateAccountRequest
+            )
         }
-        return data
     }
 
     fun addIncome(request: AccountIncomeRequest) {
-        viewModelScope.launch {
-            accountRepository.addAccountIncome(request)
-                .let {
-                    if(it.isSuccessful){
-                        Log.e("TAG", "updateAccount: SUCCESS")
-                    }else {
-                        Log.e("TAG", "updateAccount: FAILURE")
-                    }
-                }
-        }
+        executeLiveDataSingle { accountRepository.addAccountIncome(request) }
+
     }
 
     fun getIncomeTypes(): LiveData<Resource<List<IncomeType>>> {
-        val data = MutableLiveData<Resource<List<IncomeType>>>()
-        viewModelScope.launch {
-            data.postValue(Resource.loading(null))
-            accountRepository.getIncomeTypes()
-                .let {
-                    if (it.isSuccessful) {
-                        data.postValue(Resource.success(it.body()))
-                    } else {
-                        data.postValue(Resource.error(it.errorBody().toString(), null))
-                    }
-                }
-        }
-        return data
+        return executeLiveDataList { accountRepository.getIncomeTypes() }
     }
 
-    fun transferMoney(request: TransferMoneyRequest) {
-        viewModelScope.launch {
-            accountRepository.transferMoney(request)
-                .let {
-                    if (it.isSuccessful) {
-                        Log.i("TAG", "transferMoney: SUCCESS")
-                    } else {
-                        Log.e("TAG", "transferMoney: ERROR")
-                    }
-                }
-        }
+    fun transferMoney(request: TransferMoneyRequest): LiveData<Resource<UpdateAccountResponse>> {
+        return executeLiveDataSingle { accountRepository.transferMoney(request) }
     }
 }
