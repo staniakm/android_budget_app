d846fa63c60318b5d616fd11da24f2ce3e9032a4
Mariusz Staniak <mariusz.staniak@gmail.com>
2022-01-07 19:57:38 +0100

    extract click listener from account adapter

--- app/src/main/java/com/example/internetapi/ui/AccountOutcomeDetails.kt
+++ app/src/main/java/com/example/internetapi/ui/AccountOutcomeDetails.kt
@@ -1,23 +1,35 @@
 package com.example.internetapi.ui
 
+import android.R
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.internetapi.api.Resource
+import com.example.internetapi.config.AccountHolder
 import com.example.internetapi.databinding.ActivityAccountOutcomeDetailsBinding
 import com.example.internetapi.functions.errorSnackBar
 import com.example.internetapi.models.AccountInvoice
+import com.example.internetapi.models.SimpleAccount
 import com.example.internetapi.models.Status
 import com.example.internetapi.models.UpdateInvoiceAccountRequest
 import com.example.internetapi.ui.adapters.AccountExpensesAdapter
+import com.example.internetapi.ui.adapters.AccountExpensesViewHolder
+import com.example.internetapi.ui.adapters.OnItemClickedListener
 import com.example.internetapi.ui.viewModel.AccountViewModel
 import com.example.internetapi.ui.viewModel.InvoiceViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.properties.Delegates
 
 @AndroidEntryPoint
-class AccountOutcomeDetails : AppCompatActivity() {
+class AccountOutcomeDetails : AppCompatActivity(), OnItemClickedListener {
 
     private val FAILED_TO_LOAD_ACCOUNT_INVOICES = "Failed to load account invoices"
 
@@ -25,12 +37,13 @@ class AccountOutcomeDetails : AppCompatActivity() {
     private val invoiceViewModel: InvoiceViewModel by viewModels()
     private lateinit var binding: ActivityAccountOutcomeDetailsBinding
     private lateinit var adapter: AccountExpensesAdapter
+    private var accountId by Delegates.notNull<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAccountOutcomeDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        adapter = AccountExpensesAdapter()
+        adapter = AccountExpensesAdapter(this)
         binding.rvInvoices.layoutManager = LinearLayoutManager(this)
         binding.rvInvoices.adapter = adapter
 
@@ -38,9 +51,10 @@ class AccountOutcomeDetails : AppCompatActivity() {
             binding.name.text = extra.getString("name", "")
             binding.outcomeSum.text = extra.getString("outcome", "0.0")
             extra.getInt("accountId").let { accountId ->
+                this.accountId = accountId
                 accountViewModel.accountInvoices(accountId).observe(this, {
                     when (it.status) {
-                        Status.SUCCESS -> loadOnSuccess(it, accountId)
+                        Status.SUCCESS -> loadOnSuccess(it)
                         Status.ERROR -> errorSnackBar(binding.root, FAILED_TO_LOAD_ACCOUNT_INVOICES)
                         Status.LOADING -> {}
                     }
@@ -49,7 +63,7 @@ class AccountOutcomeDetails : AppCompatActivity() {
         }
     }
 
-    fun updateInvoiceAccount(invoiceId: Long, oldAccount: Int, accountId: Int) {
+    private fun updateInvoiceAccount(invoiceId: Long, oldAccount: Int, accountId: Int) {
         invoiceViewModel.updateInvoiceAccount(
             UpdateInvoiceAccountRequest(
                 invoiceId,
@@ -62,11 +76,54 @@ class AccountOutcomeDetails : AppCompatActivity() {
         }
     }
 
-    private fun loadOnSuccess(it: Resource<List<AccountInvoice>>, accountId: Int) {
+    private fun loadOnSuccess(it: Resource<List<AccountInvoice>>) {
         binding.progress.visibility = View.GONE
         binding.rvInvoices.visibility = View.VISIBLE
         it.data?.let {
-            adapter.submitList(it, accountId)
+            adapter.submitList(it)
         }
     }
+
+    override fun onClick(position: Int, element: String) {
+        val item = adapter.getItem(position)
+        when (element) {
+            "layout" -> Intent(this, InvoiceDetailsActivity::class.java).apply {
+                this.putExtra("invoiceId", item.listId)
+            }.let {
+                ContextCompat.startActivity(this, it, null)
+            }
+            "layoutLong" -> createSpinner(item)
+        }
+    }
+
+    private fun createSpinner(
+        item: AccountInvoice
+    ) {
+        val accounts = AccountHolder.accounts
+        val spinner = Spinner(this)
+        spinner.adapter = ArrayAdapter(
+            this,
+            R.layout.simple_spinner_dropdown_item,
+            accounts
+        )
+        spinner.setSelection(accounts.indexOfFirst { it.id == accountId })
+        val alert: AlertDialog.Builder = AlertDialog.Builder(this)
+        alert.setTitle("Change account for selected invoice")
+            .setMessage("${item.listId}")
+            .setView(spinner)
+            .setPositiveButton("OK") { _, i ->
+                Log.i(
+                    "TAG",
+                    "onBindViewHolder: OK ${(spinner.selectedItem as SimpleAccount).id}"
+                )
+                this.updateInvoiceAccount(
+                    item.listId,
+                    accountId,
+                    (spinner.selectedItem as SimpleAccount).id
+                )
+            }
+            .setNegativeButton("Cancel") { _, _ -> }
+        alert.show()
+    }
+
 }
--- app/src/main/java/com/example/internetapi/ui/adapters/AccountExpensesAdapter.kt
+++ app/src/main/java/com/example/internetapi/ui/adapters/AccountExpensesAdapter.kt
@@ -1,27 +1,19 @@
 package com.example.internetapi.ui.adapters
 
-import android.app.AlertDialog
 import android.content.Context
-import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.example.internetapi.config.AccountHolder
 import com.example.internetapi.config.MoneyFormatter.df
 import com.example.internetapi.databinding.InvoiceAdapterBinding
 import com.example.internetapi.models.AccountInvoice
-import com.example.internetapi.models.SimpleAccount
-import com.example.internetapi.ui.AccountOutcomeDetails
-import com.example.internetapi.ui.InvoiceDetailsActivity
 
 
-class AccountExpensesAdapter : RecyclerView.Adapter<AccountExpensesViewHolder>() {
+class AccountExpensesAdapter(private val listener: OnItemClickedListener) :
+    RecyclerView.Adapter<AccountExpensesViewHolder>() {
 
     private val diffCallback = object : DiffUtil.ItemCallback<AccountInvoice>() {
         override fun areItemsTheSame(oldItem: AccountInvoice, newItem: AccountInvoice): Boolean {
@@ -34,10 +26,8 @@ class AccountExpensesAdapter : RecyclerView.Adapter<AccountExpensesViewHolder>()
     }
 
     private val differ = AsyncListDiffer(this, diffCallback)
-    private var account: Int = -1
 
-    fun submitList(list: List<AccountInvoice>, accountId: Int) {
-        account = accountId
+    fun submitList(list: List<AccountInvoice>) {
         differ.submitList(list)
     }
 
@@ -46,7 +36,7 @@ class AccountExpensesAdapter : RecyclerView.Adapter<AccountExpensesViewHolder>()
         val binding =
             InvoiceAdapterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
-        return AccountExpensesViewHolder(binding, parent.context)
+        return AccountExpensesViewHolder(binding, parent.context, listener)
     }
 
     override fun onBindViewHolder(holder: AccountExpensesViewHolder, position: Int) {
@@ -55,53 +45,9 @@ class AccountExpensesAdapter : RecyclerView.Adapter<AccountExpensesViewHolder>()
             shopName.text = item.name
             cost.text = df.format(item.price)
             date.text = item.date
-            layout.setOnLongClickListener {
-                createSpinner(holder, item)
-                true
-            }
-            layout.setOnClickListener {
-
-                val indent = Intent(holder.parent, InvoiceDetailsActivity::class.java).apply {
-                    this.putExtra("invoiceId", item.listId.toLong())
-                }
-                ContextCompat.startActivity(holder.parent, indent, null)
-            }
         }
-
     }
 
-    private fun createSpinner(
-        holder: AccountExpensesViewHolder,
-        item: AccountInvoice
-    ) {
-        val accounts = AccountHolder.accounts
-        val spinner = Spinner(holder.parent)
-        spinner.adapter = ArrayAdapter(
-            holder.parent,
-            android.R.layout.simple_spinner_dropdown_item,
-            accounts
-        )
-        spinner.setSelection(accounts.indexOfFirst { it.id == account })
-        val alert: AlertDialog.Builder = AlertDialog.Builder(holder.parent)
-        alert.setTitle("Change account for selected invoice")
-            .setMessage("${item.listId}")
-            .setView(spinner)
-            .setPositiveButton("OK") { _, i ->
-                Log.i(
-                    "TAG",
-                    "onBindViewHolder: OK ${(spinner.selectedItem as SimpleAccount).id}"
-                )
-                (holder.parent as AccountOutcomeDetails).updateInvoiceAccount(
-                    item.listId,
-                    account,
-                    (spinner.selectedItem as SimpleAccount).id
-                )
-            }
-            .setNegativeButton("Cancel") { _, _ ->
-                Log.i("TAG", "onBindViewHolder: CANCEL")
-            }
-        alert.show()
-    }
 
     override fun getItemCount(): Int {
         return differ.currentList.size
@@ -109,10 +55,47 @@ class AccountExpensesAdapter : RecyclerView.Adapter<AccountExpensesViewHolder>()
 
     fun removeInvoice(invoiceId: Long) {
         differ.currentList.filter { it.listId != invoiceId }.let {
-            submitList(it, account)
+            submitList(it)
         }
+    }
+
+    fun getItem(position: Int): AccountInvoice {
+        return differ.currentList[position]
     }
 }
 
-class AccountExpensesViewHolder(val binding: InvoiceAdapterBinding, val parent: Context) :
-    RecyclerView.ViewHolder(binding.root) {}
+class AccountExpensesViewHolder(
+    val binding: InvoiceAdapterBinding,
+    val parent: Context,
+    private val listener: OnItemClickedListener
+) :
+    RecyclerView.ViewHolder(binding.root), View.OnClickListener, View.OnLongClickListener {
+    private val layout = binding.layout
+
+    init {
+        layout.setOnClickListener(this)
+        layout.setOnLongClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            layout.id -> bindingAdapterPosition.let {
+                if (it != RecyclerView.NO_POSITION) {
+                    listener.onClick(it, "layout")
+                }
+            }
+        }
+    }
+
+    override fun onLongClick(v: View?): Boolean {
+        when (v?.id) {
+            layout.id -> bindingAdapterPosition.let {
+                if (it != RecyclerView.NO_POSITION) {
+                    listener.onClick(it, "layoutLong")
+                }
+            }
+        }
+        return true
+    }
+}
+
